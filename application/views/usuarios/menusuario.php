6cfce935123a4375b569a751e69d5dcd9c2d1d0d
unknown <invoker0pudge@gmail.com>
2020-07-30 19:43:46 -0500

    bootstrap

--- application/views/usuarios/menusuario.php
+++ application/views/usuarios/menusuario.php
@@ -1,11 +1,11 @@
 
     <ul>
        <li><a href="<?php echo base_url() ?>">inicio</a></li>
-<<<<<<< HEAD
+
        <li><a href="<?php echo base_url('Categorias') ?>">Categorias</a></li>      
-=======
+
        <li><a href="<?php echo base_url('Categorias') ?>">Categorias</a></li>
        <li><a href="<?php echo base_url('Welcome/confidencial') ?>">Info</a></li>       
->>>>>>> 68bebaf1bed64946f9c20a3f7a4778bfc10b43c2
+
        <li><a href="<?php echo base_url('Login/salir') ?>">salir</a></li>
     </ul>
