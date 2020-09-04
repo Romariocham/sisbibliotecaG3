a852e41afaeee811c22c2655fd3a6706a07f3278
JUan Carlos Ayala <apolyota@gmail.com>
2020-09-04 02:21:29 -0500

    eliminacion de mensajes erroneo en welcome

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-<!-- <body style="background-image: url(https://i.pinimg.com/originals/6c/c0/99/6cc099776c601f985b73b15a338d393e.jpg);"> -->
-<body>
-=======
 
-<body>
 
-<body >
-
->>>>>>> 96400fc2a08e3d8d5c2704822a6319a302d6703d
   <div id="login" style="padding: 50px " >
         <div class="container" >
             <div id="login-row" class="row justify-content-center align-items-center" >
